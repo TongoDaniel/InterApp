57843fd2333f87db6315cb23de5b3022e05d3a6a
Belva Samyra YONTA KENNE <belvayonta@gmail.com>
2026-01-26 16:03:48 +0100

    Model de base

---
+++ authentication/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
+class Profile(AbstractBaseUser):
+    ROLE_CHOICES = [
+        ('client', 'Client'),
+        ('cabinet_architect', 'Cabinet d\'architecture'),
+        ('societe_immo', 'Société immobilière'),
+    ]
+    user = models.OneToOneField(AbstractBaseUser, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, blank=False, null=False)
+    profile_photo = models.ImageField(upload_to="profile_pics",  verbose_name='Photo de profil', default='photo_profil_base.jpg')
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.get_role_display()}"
+
+    def save(self, *args, **kwargs):
+        if self.role == 'client':
+           self.is_superuser = False
+           self.is_staff = False
+           self.set_password(self.password)
+        elif self.role == '':
+           self.is_superuser = True
+           self.is_staff = True
+           self.set_password(self.password)
+        else:
+           self.is_superuser = False
+           self.is_staff = True
+           self.set_password(self.password)
+
+        super().save(*args, **kwargs)
